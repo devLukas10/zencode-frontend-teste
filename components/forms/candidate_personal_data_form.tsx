ac1b7474444ecf675f0295a401f75274f8374499
devLukas10 <devlucasveraz@gmail.com>
2026-02-25 12:02:23 +0100

    file changed 8745877

--- components/forms/candidate_personal_data_form.tsx
+++ components/forms/candidate_personal_data_form.tsx
@@ -48,10 +48,7 @@ export const CadidatePersonalDataForm = ({show, next, cb}: CandidateFormTypes)=>
 
     if (!show) return <div className='hidden' />
 
-    return <form onSubmit={(e)=> submit(e)}
-        className={`w-full flex flex-col h-[200px] gap-2 mt-[20px]`}
-    >
-
+    return <div className="w-full flex flex-col h-[200px] mt-[20px]">
         <label htmlFor="input-avatar" className="m-0 mb-[35px] w-[100px] h-[100px]">
             <div className="w-[100px] h-[100px] rounded-full border border-gray-200 cursor-pointer flex items-center justify-center"
                 style={{
@@ -64,75 +61,81 @@ export const CadidatePersonalDataForm = ({show, next, cb}: CandidateFormTypes)=>
                 {!_fileView ? <LuUser size={40} color='gray' /> : ''}                
             </div>
         </label>
-
-        <InputGroup.Root
-            label={CANDIDATE_FORM_CONTETX_LABELS.lab1}
-            errorText=""
+        
+        <form onSubmit={(e)=> submit(e)}
+            className={`w-full flex flex-col h-[200px] gap-2`}
         >
-            <InputGroup.Input
-                value={_currentData?.fullname}
-                onChange={(txt)=> setCurrentData({..._currentData, fullname: txt})}
+            <InputGroup.Root
+                label={CANDIDATE_FORM_CONTETX_LABELS.lab1}
+                errorText=""
+            >
+                <InputGroup.Input
+                    value={_currentData?.fullname}
+                    onChange={(txt)=> setCurrentData({..._currentData, fullname: txt})}
 
+                    type="text"
+                    placeholder="- - - - - -"
+                    className="w-full"
+                    required={true}
+                />
+            </InputGroup.Root>
+            <InputGroup.Root
+                type="email"
+                label={CANDIDATE_FORM_CONTETX_LABELS.lab2}
+                errorText=""
+            >
+                <InputGroup.Input
+                    value={_currentData?.email}
+                    onChange={(txt)=> setCurrentData({..._currentData, email: txt})}
+
+                    type='email'
+                    placeholder="- - - - - -"
+                    className="w-full"
+                    required={true}
+                />
+            </InputGroup.Root>
+            <InputGroup.Root
                 type="text"
-                placeholder="- - - - - -"
-                className="w-full"
-                required={true}
-            />
-        </InputGroup.Root>
-        <InputGroup.Root
-            type="email"
-            label={CANDIDATE_FORM_CONTETX_LABELS.lab2}
-            errorText=""
-        >
-            <InputGroup.Input
-                value={_currentData?.email}
-                onChange={(txt)=> setCurrentData({..._currentData, email: txt})}
+                label={CANDIDATE_FORM_CONTETX_LABELS.lab3}
+                errorText=""
+            >
+                <InputGroup.Input
+                    value={_currentData?.number}
+                    onChange={(txt)=> setCurrentData({..._currentData, number: txt})}
 
-                type='email'
-                placeholder="- - - - - -"
-                className="w-full"
-                required={true}
-            />
-        </InputGroup.Root>
-        <InputGroup.Root
-            type="text"
-            label={CANDIDATE_FORM_CONTETX_LABELS.lab3}
-            errorText=""
-        >
-            <InputGroup.Input
-                value={_currentData?.number}
-                onChange={(txt)=> setCurrentData({..._currentData, number: txt})}
+                    placeholder="- - - - - -"
+                    className="w-full"
+                    required={true}
+                />
+            </InputGroup.Root>
+            <InputGroup.Root
+                type="text"
+                label={CANDIDATE_FORM_CONTETX_LABELS.lab4}
+                errorText=""
+            >
+                <InputGroup.Input
+                    value={_currentData?.address}
+                    onChange={(txt)=> setCurrentData({..._currentData, address: txt})}
+                    placeholder="- - - - - -"
+                    className="w-full"
+                    required={true}
+                />
+            </InputGroup.Root>
 
-                placeholder="- - - - - -"
-                className="w-full"
-                required={true}
-            />
-        </InputGroup.Root>
-        <InputGroup.Root
-            type="text"
-            label={CANDIDATE_FORM_CONTETX_LABELS.lab4}
-            errorText=""
-        >
-            <InputGroup.Input
-                value={_currentData?.address}
-                onChange={(txt)=> setCurrentData({..._currentData, address: txt})}
-                placeholder="- - - - - -"
-                className="w-full"
-                required={true}
-            />
-        </InputGroup.Root>
+            <ButtonGroup.CustomButton
+                className="w-full rounded-md p-3 bg-blue-500 flex items-center justify-center cursor-pointer mt-[10px] "
+                loading={_btnState.loading}
+                disabled={_btnState.disabled}
+            >
+                <span className="font-bold text-white">
+                    {CANDIDATE_FORM_CONTETX_LABELS.lab18}
+                </span>
+            </ButtonGroup.CustomButton>
 
-        <ButtonGroup.CustomButton
-            className="w-full rounded-md p-3 bg-blue-500 flex items-center justify-center cursor-pointer mt-[10px] "
-            loading={_btnState.loading}
-            disabled={_btnState.disabled}
-        >
-            <span className="font-bold text-white">
-                {CANDIDATE_FORM_CONTETX_LABELS.lab18}
-            </span>
-        </ButtonGroup.CustomButton>
+            {/***  */}
+            
+        </form>
 
-        {/***  */}
         <input
             onChange={(e)=> {
                 const res = Array.from(e.target.files as any)[0];
@@ -144,5 +147,6 @@ export const CadidatePersonalDataForm = ({show, next, cb}: CandidateFormTypes)=>
             accept="image/.png .jpg .jpeg"
             className="w-0 h-0 text-transparent p-0 bg-transparent"
         />
-    </form>
+    </div>
+    
 }
